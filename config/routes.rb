a1b260a076e56c30efdfeeea2696c730608a60da
jomariepolanco <jomariepolanco@gmail.com>
2020-10-29 21:25:12 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :songs
+  post '/songs/new', to: 'songs#create'
+  patch 'songs/:id/edit', to: 'songs#update'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
